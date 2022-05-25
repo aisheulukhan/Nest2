b86e518c5502f2522c1c6427ae77292340c92516
tu7hnsxs1 <tu7hnsxs1@code.edu.az>
2022-05-25 12:47:09 +0400

    add

---
+++ TaskNest/TaskNest/Areas/Manage/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TaskNest.Areas.Manage.Controllers
+{
+    public class ProductController : Controller
+    {
+        [Area("Manage")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
